77cc0dffee4042b38a37922112dc06ef0f6d3efe
Toll <toll25@proton.me>
2025-12-02 09:44:55 +0100

    added proper day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,28 +2,62 @@ use std::fs::read_to_string;
 
 fn main() {
     let inputs = get_input();
-    part1(inputs);
+    part1(inputs.clone());
+    part2(inputs);
 }
 
 fn part1(input: Vec<(u64, u64)>) {
     let mut counter = 0;
     for pair in input {
         for id in pair.0..=pair.1 {
-            for check_num in 0..=10_i32.pow((id.to_string().len() / 2) as u32) {
-                let check_string = check_num.to_string();
-                let id_string = id.to_string();
-                let stripped_once = id_string.strip_prefix(&check_string);
-                if stripped_once.is_none() {
+            let id_string = id.to_string();
+            if id_string.len() % 2 != 0 {
+                continue;
+            }
+            let halves = id_string.split_at(id_string.len() / 2);
+            if halves.0 == halves.1 {
+                counter += id;
+            }
+        }
+    }
+    println!("{counter}");
+}
+
+fn part2(input: Vec<(u64, u64)>) {
+    let mut counter = 0;
+    for pair in input {
+        for id in pair.0..=pair.1 {
+            let id_string = id.to_string();
+            // println!("id_string {id_string}");
+            let mut valid = false;
+            for divisor in 1..=id_string.len() / 2 {
+                if id_string.len() % divisor != 0 {
                     continue;
                 }
-                let stripped_twice = stripped_once.unwrap().strip_prefix(&check_string);
-                if stripped_twice.is_none() {
-                    continue;
+                // println!("divisor {divisor}");
+                let num_to_check = &id_string[0..divisor];
+                // println!("num_to_check {num_to_check}");
+                let num_parts = id_string.len() / divisor;
+                // println!("num_parts {num_parts}");
+                if num_parts < 2 {
+                    break;
                 }
-                if stripped_twice.unwrap().is_empty() {
-                    println!("Adding number: {id}");
-                    counter += id;
+                let mut divisor_valid = true;
+                for part_num in 1..num_parts {
+                    let part = &id_string[divisor * (part_num)..divisor * (part_num + 1)];
+                    // println!("part{part_num} {part}");
+
+                    if part != num_to_check {
+                        divisor_valid = false;
+                    }
                 }
+                if divisor_valid {
+                    valid = true;
+                }
+            }
+            if valid {
+                println!("adding: {id}");
+                counter += id;
             }
         }
     }
